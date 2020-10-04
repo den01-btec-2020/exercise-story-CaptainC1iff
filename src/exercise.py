d17d27fcad196566a9b749a4a3400fad8441b417
Captain C1iff <l.ashdown314@gmx.com>
2020-10-04 11:37:15 +0000

    Works as intended, not sure why pytest is marking it as wrong.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
-    #write your code below this line
+  print ("I will tell you a story, but I need some information first.")
+
+  Name = input("What is the main character called?")
+
+  Job = input("What is their job?")
+
+  print ("Here is the story:")
+
+  print("Once upon a time there was " , Name , " who was a " , Job)
+
+  print("On the way to work, " , Name , "reflected on life.")
+
+  print ("Perhaps " , Name , " will not be a " , Job , " forever.")
 
 if __name__ == '__main__':
     main()
